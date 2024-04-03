ab58c3238f051696f25adac57ea8411aea155839
LEONARDO <leushu@gmail.com>
2024-04-03 09:58:06 -0300

    create themes and global styles

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import styles from './App.module.css';
+import { Header } from './components/Header';
+
+
+export function App() {
+  return (
+    <div className={styles.container}>
+      <Header/>
+    </div>
+    
+  )
+}
+
